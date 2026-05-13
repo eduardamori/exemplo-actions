5f31becd2b7c656591868b14a07a9dff577c2676
eduardamori <eduarda.almeida@sptech.school>
2026-05-12 21:35:42 -0300

    first commit

---
+++ src/test/java/sptech/schools/exemplo_actions/ExemploActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package sptech.schools.exemplo_actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
