9515cbccccbf505a19c89107a1b9659d36920a9a
eduardamori <eduarda.almeida@sptech.school>
2026-05-15 18:39:27 -0300

    Adicionando testes

---
+++ src/test/java/sptech/schools/exemplo_actions/service/CalculadoraServiceTest.java
@@ -0,0 +1,24 @@
+package sptech.schools.exemplo_actions.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculadoraServiceTest {
+
+    @Test
+    @DisplayName("Quando acionado com 10 e 2, então deve retornar 5")
+    public void testDividir() {
+
+        CalculadoraService calculadoraService = new CalculadoraService();
+        double a = 10;
+        double b = 2;
+        double esperado = 5;
+
+        double resultado = calculadoraService.dividir(a, b);
+
+        assertEquals(esperado, resultado);
+    }
+
+}
